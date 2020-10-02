49495f462c1f1a6ea911c02ccf8839e648d82614
Jacob Callear <jacob18@callear.net>
2020-10-02 14:27:24 +0100

    Add test for splitting basket in raw data

--- tests/test_data_handling.py
+++ tests/test_data_handling.py
@@ -4,8 +4,8 @@ from io import StringIO
 
 import pytest
 
-from src.classes import RawTransaction
-from src.data_handling import to_raw_transactions
+from src.classes import RawTransaction, Basket
+from src.data_handling import to_raw_transactions, split_basket
 
 def test_to_raw_transactions():
     '''Test `to_raw_transactions` function returns class with correct attributes.
@@ -34,3 +34,21 @@ def test_to_raw_transactions():
     assert actual_output.pay_amount == expected_output.pay_amount
     assert actual_output.payment_method == expected_output.payment_method
     assert actual_output.ccn == expected_output.ccn
+
+def test_split_basket():
+    # Arrange
+    example_basket = "Regular Luxury hot chocolate - £2.40, Regular Flavoured hot chocolate - Hazelnut - £2.60"
+    expected_output = [
+        Basket(item='Regular Luxury hot chocolate',
+               cost='£2.40'),
+        Basket(item='Regular Flavoured hot chocolate - Hazelnut',
+               cost='£2.60')
+    ]
+    # Act
+    actual_output = list(split_basket(example_basket))
+    # Assert
+    assert len(expected_output) == len(actual_output)
+
+    for actual_basket, expected_basket in zip(actual_output, expected_output):
+        assert actual_basket.item == expected_basket.item
+        assert actual_basket.cost == expected_basket.cost
