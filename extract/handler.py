e221784dbc0836329ab2d7221fc6fb101ea50566
Shameela8 <70574103+Shameela8@users.noreply.github.com>
2020-10-14 11:47:08 +0100

    Update handler.py

--- extract/handler.py
+++ extract/handler.py
@@ -1,3 +1,32 @@
-
+import os
+import sys
+import boto3
+import json
+from read_from_s3 import get_file_names, read_csv_file_from_s3, output_raw_transactions
+from send_dict_to_sqs import send_dict_to_sqs 
 def start(event, context):
-    return None
+    try:
+        all_files = get_file_names()
+    except Exception as ERROR:
+        print ("Couldn't extract from S3 files")
+        print (str(ERROR))
+    for file_name in all_files:
+        try:
+            # Read data
+            data = read_csv_file_from_s3(bucket="group3-testbucket", key=file_name)
+            print('Read data from csv')
+            raw_transactions = output_raw_transactions(data)
+            print('Read raw transactions')
+            json_data = json.dumps(raw_transactions)
+            print('Converted to JSON')
+        except Exception as ERROR:
+            print ("Couldn't extract from S3 files")
+            print (str(ERROR))
+          
+        try:
+          send_dict_to_sqs(json_data)
+          print(f"{file_name} : data was sent to SQS")
+        except Exception as ERROR:
+          print(f"{file_name} : data was not sent to SQS")
+          print(str(ERROR))
+            
