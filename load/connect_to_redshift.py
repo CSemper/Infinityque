e09fee868cbde9bc3f631889933a888d3440f20e
Jacob Callear <jacob18@callear.net>
2020-10-15 10:15:53 +0100

    Add module to load data into redshift

---
+++ load/connect_to_redshift.py
@@ -0,0 +1,45 @@
+import os
+import sys
+
+import boto3
+import psycopg2
+from dotenv import load_dotenv
+
+def connect_to_redshift():
+    '''Returns connection to a RedShift database using credentials in environment.'''
+    load_dotenv()
+    print ("Getting credentials...")
+    # Get connection credentials
+    host = os.getenv("DB_HOST")
+    port = int(os.getenv("DB_PORT"))
+    user = os.getenv("DB_USER")
+    passwd = os.getenv("DB_PASS")
+    db = os.getenv("DB_NAME")
+    cluster = os.getenv("DB_CLUSTER")
+
+    try:
+        client = boto3.client('redshift')
+        creds = client.get_cluster_credentials(  # Lambda needs these permissions as well DataAPI permissions
+            DbUser=user,
+            DbName=db,
+            ClusterIdentifier=cluster,
+            DurationSeconds=3600) # Length of time access is granted
+    except Exception as ERROR:
+        print("Credentials Issue: " + str(ERROR))
+        sys.exit(1)
+
+    print('Got credentials')
+
+    # Connect to RedShift
+    try:
+        conn = psycopg2.connect(
+            dbname=db,
+            user=creds["DbUser"],
+            password=creds["DbPassword"],
+            port=port,
+            host=host)
+    except Exception as ERROR:
+        print("Connection Issue: " + str(ERROR))
+        sys.exit(1)
+    print('Connected to RedShift')
+    return conn
