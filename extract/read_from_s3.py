ec75c138c5c14ea8a635c20f14510ff2f4da3adc
Chenyse Semper <chenyse.semper@hotmail.com>
2020-10-17 21:45:48 +0100

    Reverted code to read files and extract  data for  all files from yesterday's folder

--- extract/read_from_s3.py
+++ extract/read_from_s3.py
@@ -14,11 +14,10 @@ def get_file_names():
     s3 = boto3.resource('s3')
     bucket = s3.Bucket("cafe-transactions")
     all_files = (file.key for file in bucket.objects.all())
-    return all_files
-    # file_name_start = get_key_prefix()
-    # for file_name in all_files:
-    #     if file_name.startswith(file_name_start):
-    #         yield file_name
+    file_name_start = get_key_prefix()
+    for file_name in all_files:
+        if file_name.startswith(file_name_start):
+            yield file_name
 
 def get_key_suffix(file_name):
     '''Returns last 12 characters of file name.'''
