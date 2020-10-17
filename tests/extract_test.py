547ffbb159a38f6783b3916f21a51bdde7a28cba
Jacob Callear <jacob18@callear.net>
2020-10-17 11:45:39 +0100

    Add test for split_long_list function

--- tests/extract_test.py
+++ tests/extract_test.py
@@ -1,13 +1,14 @@
 '''Test extract module.
 
-Tests 1 function:
+Tests 2 functions:
 - output_raw_transactions
+- split_long_list
 '''
 
 import csv
 from io import StringIO
 
-from extract.read_from_s3 import output_raw_transactions
+from extract.read_from_s3 import output_raw_transactions, split_long_list
 
 def test_output_raw_transactions():
     '''Test `output_raw_transactions` returns list of transaction dictionaries.
@@ -34,4 +35,19 @@ def test_output_raw_transactions():
     actual_output = output_raw_transactions(example_csv, identifier=example_identifier, skip_header=False)
     # ASSERT
     assert len(actual_output) == len(expected_output)
+    assert actual_output == expected_output
+
+def test_split_long_list():
+    # Arrange
+    example_input_list = list(range(10))
+    max_length = 3
+    expected_output = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [9]
+    ]
+    # Act
+    actual_output = split_long_list(example_input_list, max_length=max_length)
+    # Assert
     assert actual_output == expected_output
