570f64cb16b2c44c8feffce2d5dab9b428815ef2
Jacob Callear <jacob18@callear.net>
2020-10-29 10:32:44 +0000

    Add data handling and messaging to kinesis extract

--- kinesislambda/handler.py
+++ kinesislambda/handler.py
@@ -1,29 +1,52 @@
-import json, logging, os
+import json
+import logging
+
 import boto3
 
-from sqs_messaging import send_message_list_to_sqs, split_long_list
+from sqs_messaging import send_sqs_message
 
-# logging.getLogger().setLevel(logging.ERROR)
+logging.getLogger().setLevel(logging.INFO)
 
 def start(event, context):
-    stream_name = 'store-stream'
-    kinesis_client = boto3.client('kinesis', region_name='eu-west-1')
-    response = kinesis_client.describe_stream(StreamName=stream_name)
-    shard_id = response['StreamDescription']['Shards'][0]['ShardId']
-    shard_iterator = kinesis_client.get_shard_iterator(StreamName=stream_name,
-                                                      ShardId=shard_id,
-                                                      ShardIteratorType='TRIM_HORIZON')
-
-    kinesis_shard_iterator = shard_iterator['ShardIterator']
-
-    record_response = kinesis_client.get_records(ShardIterator=kinesis_shard_iterator,Limit=5)
- 
-    while 'NextShardIterator' in record_response:
-        # read a certain number of records at a time from the shard number
-        record_response = kinesis_client.get_records(ShardIterator=record_response['NextShardIterator'],Limit=5)
-        print (record_response['Records'])
-        print ("Okay")
-        print (record_response['Records'][0]['Data'])
-        # time.sleep(1) - sleep before continuing while loop
-        
-        
+    # Get data from kinesis stream
+    kinesis_stream = event['Records']
+    counter = 0
+    for data_record in kinesis_stream:
+        # Convert binary string to python list
+        row_binary = data_record['Data']
+        logging.info({'binary row': row_binary})
+        row_string = row_binary.decode('utf-8')
+        logging.info({'decoded row': row_string})
+        row = json.loads(row_string)
+        logging.info({'row': row})
+        # Create unique identifier
+        identifier = row[1].ljust(12, 'a')
+        # Convert list to dictionary
+        raw_transaction = {
+            'date': row[0],
+            'location': row[1],
+            'customer_name': row[2],
+            'basket': row[3],
+            'pay_amount': row[4],
+            'payment_method': row[5],
+            'ccn': row[6],
+            'id_number': counter,
+            'identity': identifier
+        }
+        logging.info({'raw transaction': raw_transaction})
+        counter += 1
+        # Convert dictionary to json list
+        message = json.dumps([raw_transaction])
+        # Send row SQS
+        queue_name = 'Group3SQSExtracttoTransform'
+        queue_url = 'https://sqs.eu-west-1.amazonaws.com/579154747729/Group3SQSExtracttoTransform'
+        try:
+            send_sqs_message(
+                message,
+                queue_name=queue_name,
+                queue_url=queue_url
+            )
+            logging.info({'message': message})
+        except Exception as ERROR:
+            logging.error({'message failed': message})
+            logging.error(ERROR)
